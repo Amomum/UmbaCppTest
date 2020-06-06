c73e1a6ed2a1943f0df3b8e94ba4bf0ab4271e56
Amomum <amomum@yandex.ru>
2020-06-06 22:51:01 +0300

    Билд больше не валится, потому что тесты падают специально

--- example_project/run_tests.sh
+++ example_project/run_tests.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-set -e
-set -x
-
-ls -la
-
 /app/eclipse/eclipse -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -import ./example_project/ -build UmbaCppTest/Docker
 
 ./example_project/Docker/UmbaCppTest
+
+retVal=$?
+if [ $retVal -eq 0 ]; then
+    echo "This test run is expected to fail!"
+    exit 1
+else 
+    echo "This test run was expected to fail to demonstrate error printing, so build status is not affected"
+    exit 0
+fi
